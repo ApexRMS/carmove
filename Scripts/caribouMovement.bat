46316b29a19d4f330f74ba2921b0ca8f5d2443e2
alex <alexe_webmail@yahoo.com>
2018-09-27 14:21:26 -0400

    Use StartProcess executable to suppress message box on Python exit for CMM runs

--- Scripts/caribouMovement.bat
+++ Scripts/caribouMovement.bat
@@ -5,7 +5,7 @@ REM Use %~dp0 to get batch file directory
 cd /d %~dp0
 REM To trap Standard error, in the same directory as the input file resides.
 set OP_PATH=%~dp0
-python caribouMovement.py %1 %2 > "%OP_PATH%caribouMovement.log" 2>&1
+StartProcess --name=python.exe --no-error-box --args="caribouMovement.py "%1" "%2"" > "%OP_PATH%caribouMovement.log" 2>&1
 rem python caribouMovement.py %1 %2
 if %ERRORLEVEL% == -1073741819 (
 	exit /b 0
